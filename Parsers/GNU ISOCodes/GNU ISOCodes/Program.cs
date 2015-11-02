0c6cfeb747daef9f73fa130ed49a713f71335ef4
DARKGuy <darkguy2008@gmail.com>
2015-11-02 03:44:56 -0430

    Added Qube submodule for using its XML serialization extensions. Added new data batch with translated country names and codes up to 2015-11-02. GNU ISOCodes app finished (albeit with a small states bug, check Application.cs near the end).

--- Parsers/GNU ISOCodes/GNU ISOCodes/Program.cs
+++ Parsers/GNU ISOCodes/GNU ISOCodes/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Qube.Extensions;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,44 +13,10 @@ namespace GNU_ISOCodes
     {
         public static string AppPath { get { return new FileInfo(Assembly.GetEntryAssembly().Location).DirectoryName + "\\"; } }
 
-        public class Country
-        {
-            public string Code { get; set; }
-            public string Alpha2 { get; set; }
-            public string Alpha3 { get; set; }
-            public string Name { get; set; }
-            public string CommonName { get; set; }
-            public string OfficialName { get; set; }
-        }
-
         static void Main(string[] args)
         {
-
-            string isocodesPath = @"";
-            string countryFile = isocodesPath + @"\iso_3166\iso_3166.xml";
-
-            XDocument doc = XDocument.Load(countryFile);
-
-            var entries =
-                from x in (
-                    from y in doc.Descendants("iso_3166_entries").Descendants("iso_3166_entry")
-                    select new Country
-                    {
-                        Code = y.Attribute("numeric_code").Value,
-                        Alpha2 = y.Attribute("alpha_2_code").Value,
-                        Alpha3 = y.Attribute("alpha_3_code").Value,
-                        Name = y.Attribute("name").Value,
-                        CommonName = y.Attribute("common_name") == null ? String.Empty : y.Attribute("common_name").Value,
-                        OfficialName = y.Attribute("official_name") == null ? String.Empty : y.Attribute("official_name").Value
-                    }
-                )
-                orderby x.Code
-                select x;
-
-            List<Country> Countries = new List<Country>();
-            Countries.AddRange(entries.ToList());
-
-
+            Application app = new Application();
+            app.Main(args);
         }
     }
 }
